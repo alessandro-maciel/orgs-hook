e7a89ea7555194a6b1e52fae19195a8fd46bba4d
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-03-02 21:38:38 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Home from "./src/telas/Home";
+
+export default function App(){
+  return <SafeAreaView style={{flex: 1}}>
+      <Home />
+  </SafeAreaView>
+}
